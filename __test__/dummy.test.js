e27b9283493741e4cf5551fad6c3a7152232d79b
Ashish Sharma <ashish8389sharma@gmail.com>
2026-08-21 10:32:09 +0530

    test: add new unit test suites for user and sum functionality

--- __test__/dummy.test.js
+++ __test__/dummy.test.js
@@ -5,4 +5,8 @@ describe("sum function", () => {
     test('adds 1+2 to rqual 3', () => {
         expect(sum(1, 2)).toBe(3);
     })
+
+    test("check if 2+2 is equal to 4", () => {
+        expect(2 + 2).toBe(4);
+    })
 })
