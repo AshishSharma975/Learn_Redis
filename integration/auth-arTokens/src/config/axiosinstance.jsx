2ca7a428fe92995e1e52e68ad6fb7e37244fbd19
Ashish Sharma <ashish8389sharma@gmail.com>
2026-09-01 14:05:06 +0530

    feat: configure axios instance with base URL and response interceptor for 401 error handling

--- integration/auth-arTokens/src/config/axiosinstance.jsx
+++ integration/auth-arTokens/src/config/axiosinstance.jsx
@@ -12,13 +12,20 @@ export let axiosInstance = axios.create({
 axiosInstance.interceptors.response.use(
     (response) => {
         console.log("this is response interceptor -> " ,response)
-        return response
+        return response  // important step for successful response
+        // always return response in response interceptor
+        // otherwise it will not send data to the component
+
     },
-        
+
+    // Error Handler
     (error) => {
         if(error.response.status == 401){
             console.log("error in interceptor -> 401");
         }
+        return Promise.reject(error); // important step for error handling
+        // always return error in error handler
+        // otherwise it will not send error to the component
         
     }
 )
