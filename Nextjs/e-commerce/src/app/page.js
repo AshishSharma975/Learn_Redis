03ad7c6fdb87a672eafdb21942eb4c02ff3a4974
Ashish Sharma <ashish8389sharma@gmail.com>
2026-09-06 14:54:59 +0530

    feat: initialize home page component for e-commerce website

--- Nextjs/e-commerce/src/app/page.js
+++ Nextjs/e-commerce/src/app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className=' flex items-center justify-center h-screen w-screen bg-gray-600'>hello e-commerce</div>
+    <div className=' flex items-center justify-center h-screen w-screen bg-gray-600'>hello e-commerce webiste</div>
   )
 }
 
